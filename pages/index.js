cb121052d270536f4152bffa47f9c095b89ad3fc
David Paul <enlightenedpie@gmail.com>
2020-08-18 21:02:02 -0500

    completely redoing my site; initial commit

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const year = (new Date).getFullYear()
+
+const Home = props => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{props.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">{props.mysite}</a>
+        </h1>
+
+        <p className={styles.description}>
+          Come back later, I'm still
+          <code className={styles.code}>npm update</code> 'ing this bad boy
+        </p>
+      </main>
+      
+      <footer className={styles.footer}>&copy; {year} Dave</footer>
+    </div>
+  )
+}
+
+export default Home
+
+export async function getStaticProps() {
+  const configData = await import(`../data/config.json`)
+  return {
+    props: {...configData},
+  }
+}
---
+++ posts/i-built-my-website-with-nextjs.md
@@ -0,0 +1,7 @@
+---
+title: "I built my website with NextJS. So what's next?"
+author: David Paul Crouch
+date: 2020-08-17
+hero_image: ../public/dave.jpg
+---
+I built my website with NextJS. So what's next?
